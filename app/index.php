8d16bc6dc5674ca64cafe4eb19eab3da373c4d89
guille209 <gguillemiranda@gmail.com>
2015-04-26 23:36:20 +0200

    [CU: Login] Primera versión del index.php y los controllers

--- app/index.php
+++ app/index.php
@@ -1,16 +1,29 @@
 <?php
 
 require '../vendor/autoload.php';
+include '/controllers/loginController.php';
+
+session_cache_limiter(false);
+session_start();
+
 $app = new \Slim\Slim();
-
-
-//$app->get('/', 'hellodefault');
-
 $app->get('/hello/:name', 'hellodefault');
-
+$app->get('/login', 'login');
+$app->get('/home', 'home');
 $app->run();
 
 function hellodefault($name) {
+    echo "Hello " . $name;
+}
 
-    echo "Hello ".$name;
+function login() {
+    $logInController = new controllers\loginController();
+    $response = $logInController->login();
+    echo $response; 
+}
+
+function home() {   
+    $logInController = new controllers\loginController();
+    $response = $logInController->home();
+    echo $response;
 }
--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
     'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
     'Slim' => array($vendorDir . '/slim/slim'),
     'Doctrine\\ORM\\' => array($vendorDir . '/doctrine/orm/lib'),
