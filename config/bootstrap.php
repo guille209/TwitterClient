45d2e11981fffb90dc97e6c5839b4f56ccdf427f
guille209 <gguillemiranda@gmail.com>
2015-05-13 14:02:17 +0200

    Falta generar .class a partir de orm

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -15,5 +15,14 @@ $path = array("yml");
 $isDevMode = false;
  
 $config = Setup::createYAMLMetadataConfiguration($path, $isDevMode);
-$config = 
+/*
+$config = new \Doctrine\ORM\Configuration();
+$route = dirname(__DIR__);
+
+$driverImpl = $config->newDefaultAnnotationDriver(array($route.DIRECTORY_SEPARATOR."app".DIRECTORY_SEPARATOR."models".DIRECTORY_SEPARATOR."entities"));
+$config->setMetadataDriverImpl($driverImpl);
+
+$config->setProxyDir(__DIR__ . '/Proxies');
+$config->setProxyNamespace('Proxies');
+*/
 $entityManager = EntityManager::create($dbParams, $config);
