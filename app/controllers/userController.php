8aaf76b1a937d3fb6802ad62f81560b6f704873a
Maria <Maria@Maria-PC>
2015-06-16 11:35:20 +0200

    showFriends UserController add.OK.

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -18,7 +18,7 @@ class userController implements interfaces\iUserController {
     function showProfile() {
         $profile = $_SESSION["connection"]->get("users/show", array("screen_name" => $_SESSION["access_token"]["screen_name"]));
         $json_string = json_encode($profile, JSON_UNESCAPED_SLASHES);
-        //var_dump($profile);
+//var_dump($profile);
         return $json_string;
     }
 
@@ -26,14 +26,20 @@ class userController implements interfaces\iUserController {
         $raw_response = $_SESSION["connection"]->post("friendships/create/" . $user_id);
         $app = \Slim\Slim::getInstance();
         $json_string = json_encode($raw_response, JSON_UNESCAPED_SLASHES);
-        //return $json_string;
+//return $json_string;
         $app->redirect('/user/showFriends');
     }
 
-    function showFriends(){
-        $raw_response = $_SESSION["connection"]->get("friends/list" , array("screen_name" => $_SESSION["access_token"]["screen_name"]) );
+    function showFriends() {
+        //$screen_name = $_SESSION["access_token"]["screen_name"];
+        $raw_response = $_SESSION["connection"]->get("friends/list", array($_SESSION["access_token"]["screen_name"]));
         $app = \Slim\Slim::getInstance();
         $json_string = json_encode($raw_response, JSON_UNESCAPED_SLASHES);
-        $json_string;
+        return $json_string;
     }
+
+    function unfollow($user_id) {
+        
+    }
+
 }
