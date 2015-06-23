cb6f3b3336483fa445c682cd7d82e41da7e2073d
guille209 <gguillemiranda@gmail.com>
2015-06-23 11:18:46 +0200

    Aplicado patron Singleton a los daos de las tecnologias en factoryDao

--- app/models/daos/FactoryDao.php
+++ app/models/daos/FactoryDao.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace models\daos;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,20 +14,29 @@ namespace models\daos;
  * @author Propietario
  */
 abstract class FactoryDao {
+
     //put your code here
-    
+
     const DOCTRINE_FACTORY = 1;
-    
-    abstract function  getUserDao();
+
+    static $doctrineFactoryDao;
+
+    abstract function getUserDao();
+
     abstract function getTweetDao();
+
     abstract function getHashtaglistDao();
-    
-    static function getFactory($indexFactory){
-        switch($indexFactory){
+
+    static function getFactory($indexFactory) {
+        switch ($indexFactory) {
             case self::DOCTRINE_FACTORY:
-                return new DoctrineFactoryDao();
+                if (!isset(self::$doctrineFactoryDao)) {
+                    self::$doctrineFactoryDao = new DoctrineFactoryDao();
+                }
+                return self::$doctrineFactoryDao;
             default:
                 throw new IllegalArgumentException();
         }
     }
+
 }
