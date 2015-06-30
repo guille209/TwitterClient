777b285bc358a29185e4a0aaf519c4e2a3b20595
guille209 <gguillemiranda@gmail.com>
2015-06-30 18:29:05 +0200

    Actualizada base de datos de la carpeta DB

--- db/twitterclient.sql
+++ db/twitterclient.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-06-2015 a las 13:00:41
+-- Tiempo de generación: 30-06-2015 a las 18:21:52
 -- Versión del servidor: 5.6.20
 -- Versión de PHP: 5.6.3
 
@@ -30,7 +30,14 @@ CREATE TABLE IF NOT EXISTS `hashtaglist` (
 `hashtaglist_id` int(11) NOT NULL,
   `user_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `hashtag` varchar(256) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Volcado de datos para la tabla `hashtaglist`
+--
+
+INSERT INTO `hashtaglist` (`hashtaglist_id`, `user_id`, `hashtag`) VALUES
+(5, '13', '#4ddddd');
 
 -- --------------------------------------------------------
 
@@ -43,15 +50,15 @@ CREATE TABLE IF NOT EXISTS `tweet` (
   `user_id` varchar(152) NOT NULL,
   `text` varchar(256) NOT NULL,
   `date` datetime NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
 -- Volcado de datos para la tabla `tweet`
 --
 
 INSERT INTO `tweet` (`tweet_id`, `user_id`, `text`, `date`) VALUES
-(15, '4', 'Mi primer tweet programado!', '1992-12-01 16:01:00'),
-(16, '4', 'ya funciona todo!', '2015-06-08 12:55:00');
+(13, '12', 'ya funciona todo!hhh', '2015-06-23 10:56:00'),
+(14, '12', 'ya funciona todo!hhh', '2015-06-23 10:56:00');
 
 -- --------------------------------------------------------
 
@@ -63,14 +70,15 @@ CREATE TABLE IF NOT EXISTS `user` (
 `user_id` int(11) NOT NULL,
   `oauth_token` varchar(256) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `oauth_token_secret` varchar(256) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Volcado de datos para la tabla `user`
 --
 
 INSERT INTO `user` (`user_id`, `oauth_token`, `oauth_token_secret`) VALUES
-(4, '323982065-69nCKYqMOqZ0LYPjwlkcdtwf6UTBI6C6CpxOM0uu', '16ijGDzOgRfz1Tmzjgb8HXjBnVqv38H2VINrBgGq051PW');
+(12, '323982065-69nCKYqMOqZ0LYPjwlkcdtwf6UTBI6C6CpxOM0uu', '16ijGDzOgRfz1Tmzjgb8HXjBnVqv38H2VINrBgGq051PW'),
+(13, '323982065-69nCKYqMOqZ0LYPjwlkcdtwf6UTBI6C6CpxOM0uu', '16ijGDzOgRfz1Tmzjgb8HXjBnVqv38H2VINrBgGq051PW');
 
 --
 -- Índices para tablas volcadas
@@ -102,17 +110,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `hashtaglist`
 --
 ALTER TABLE `hashtaglist`
-MODIFY `hashtaglist_id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `hashtaglist_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de la tabla `tweet`
 --
 ALTER TABLE `tweet`
-MODIFY `tweet_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+MODIFY `tweet_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
